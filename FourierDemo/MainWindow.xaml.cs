900c6986d0fad99d96de2825596ed6c54d4ac089
Matt Ford <zeotherm@gmail.com>
2021-03-19 20:09:45 -0400

    Initial checkin

--- FourierDemo/MainWindow.xaml.cs
+++ FourierDemo/MainWindow.xaml.cs
@@ -12,14 +12,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FourierDemo {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        System.Timers.Timer timer = new System.Timers.Timer(1000);
+        private double theta = 0;
+        private readonly double x_0, y_0, r, deg2rad; // Where the origin is
         public MainWindow() {
             InitializeComponent();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Enabled = false;
+            x_0 = testLine.X1;
+            y_0 = testLine.Y1;
+            r = testLine.X2 - testLine.X1;
+            deg2rad = Math.PI / 180;
+        }
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e) {
+            this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
+            {
+                //secondHand.Angle = DateTime.Now.Second * 6;
+                //minuteHand.Angle = DateTime.Now.Minute * 6;
+                //hourHand.Angle = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5);
+            }));
+        }
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e) {
+            this.DragMove();
+        }
+
+        private void AdvButton_Click(object sender, RoutedEventArgs e) {
+            theta = (theta-10*deg2rad) % 360;
+            var x = x_0 + r * Math.Cos(theta);
+            var y = y_0 + r * Math.Sin(theta);
+            testLine.X2 = x;
+            testLine.Y2 = y;
+
         }
     }
 }
